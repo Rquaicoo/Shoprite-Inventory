79164dbf4b885330d588814508c78c3d5ea0e3aa
Rquaicoo <russellquaicoo1@gmail.com>
2022-08-25 15:45:48 +0000

    logic for use cases

---
+++ Shoprite Ghana Limited/ViewModel/Till.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoprite_Ghana_Limited.ViewModel
+{
+    internal class Till
+    {
+    }
+}
