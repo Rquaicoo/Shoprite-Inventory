bd2a4e0e7d5237e6fabf1f1091eab9af70fd13fa
Rquaicoo <russellquaicoo1@gmail.com>
2022-08-27 09:52:49 +0000

    interface for managing categories

---
+++ Shoprite Ghana Limited/View/CategoryManagement/ManageCategories.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Shoprite_Ghana_Limited.View.CategoryManagement
+{
+    /// <summary>
+    /// Interaction logic for ManageCategories.xaml
+    /// </summary>
+    public partial class ManageCategories : Window
+    {
+        public ManageCategories()
+        {
+            InitializeComponent();
+        }
+    }
+}
