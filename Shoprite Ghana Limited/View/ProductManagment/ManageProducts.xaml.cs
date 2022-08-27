03598a33b0d87240004f87133a9f3734650b6aee
Rquaicoo <russellquaicoo1@gmail.com>
2022-08-27 10:01:45 +0000

    bind combobox to products table

---
+++ Shoprite Ghana Limited/View/ProductManagment/ManageProducts.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using Shoprite_Ghana_Limited.View;
+
+namespace Shoprite_Ghana_Limited.View.ProductManagment
+{
+    /// <summary>
+    /// Interaction logic for ManageProducts.xaml
+    /// </summary>
+
+    
+    public partial class ManageProducts : Window
+    {
+
+        SqlConnection connection = new SqlConnection();
+        public ManageProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string query = "SELECT * FROM categories;";
+
+            MySqlCommand cmd = new MySqlCommand(query, connection.get_connection());
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                comboBox.Items.Add(reader.GetString(0));
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "SELECT * FROM categories;";
+
+            MySqlCommand cmd = new MySqlCommand(query, connection.get_connection());
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                comboBox.Items.Add(reader.GetString("name"));
+
+            }
+        }
+    }
+}
