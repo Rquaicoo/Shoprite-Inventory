5c62ad6f740dac298d36edfe0fa37ad2da0b8c0f
Rquaicoo <russellquaicoo1@gmail.com>
2022-08-27 09:56:36 +0000

    interface for shock page; show display box for management

---
+++ Shoprite Ghana Limited/View/Pages/Stock.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Shoprite_Ghana_Limited.View.StockManagement;
+
+namespace Shoprite_Ghana_Limited.View.Pages
+{
+    /// <summary>
+    /// Interaction logic for Stock.xaml
+    /// </summary>
+    public partial class Stock : Page
+    {
+        public Stock()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ManageStock manageStock = new ManageStock();
+            manageStock.Show();
+        }
+    }
+}
