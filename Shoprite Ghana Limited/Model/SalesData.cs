927ffb3a5974b38b61947820fe02e7db1efe7a89
Rquaicoo <russellquaicoo1@gmail.com>
2022-09-03 23:35:52 +0000

    data model for dashboard

---
+++ Shoprite Ghana Limited/Model/SalesData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoprite_Ghana_Limited.Model
+{
+    public class SalesData
+    {
+        public int sales { get; set; }
+        public string month { get; set; }
+       
+    }
+}
