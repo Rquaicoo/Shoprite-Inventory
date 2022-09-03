f3cf1697cdd22c8c60c6247c3515ef7cdba5ab5e
Rquaicoo <russellquaicoo1@gmail.com>
2022-09-03 23:38:39 +0000

    load category data from database

--- Shoprite Ghana Limited/View/Pages/Categories.xaml.cs
+++ Shoprite Ghana Limited/View/Pages/Categories.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 using Shoprite_Ghana_Limited.View.CategoryManagement;
 
 namespace Shoprite_Ghana_Limited.View.Pages
@@ -21,15 +23,45 @@ namespace Shoprite_Ghana_Limited.View.Pages
     /// </summary>
     public partial class Categories : Page
     {
+
+        SqlConnection connection = new SqlConnection();
         public Categories()
         {
             InitializeComponent();
+            BindData();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             ManageCategories manageCategories = new ManageCategories();
             manageCategories.Show();
+
+        }
+
+        private void BindData()
+        {
+            //fetch data from database
+            string sql = "SELECT * FROM categories;";
+            connection.openConnection();
+
+            try
+            {
+                MySqlDataAdapter cmd = new MySqlDataAdapter(sql, connection.get_connection());
+                DataSet ds = new DataSet();
+                cmd.Fill(ds, "CategoryDataBinding");
+                CategoryDataGrid.DataContext = ds;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+            }
+
+
+        }
+
+        private void UserDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
         }
     }
