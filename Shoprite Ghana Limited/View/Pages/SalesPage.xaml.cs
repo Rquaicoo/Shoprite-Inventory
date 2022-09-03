8436c7dda3ab4182276992603d6ac9aa24a015fd
Rquaicoo <russellquaicoo1@gmail.com>
2022-09-03 23:39:27 +0000

    display sales data on sales datagrid

---
+++ Shoprite Ghana Limited/View/Pages/SalesPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Shoprite_Ghana_Limited.View;
+
+namespace Shoprite_Ghana_Limited.View.Pages
+{
+    /// <summary>
+    /// Interaction logic for Sales.xaml
+    /// </summary>
+    public partial class SalesPage : Page
+    {
+        SqlConnection connection = new SqlConnection();
+        public SalesPage()
+        {
+            InitializeComponent();
+            BindData();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BindData()
+        {
+            //fetch data from database
+            string sql = "SELECT * FROM sales;";
+            connection.openConnection();
+
+            try
+            {
+                MySqlDataAdapter cmd = new MySqlDataAdapter(sql, connection.get_connection());
+                DataSet ds = new DataSet();
+                cmd.Fill(ds, "SaleDataBinding");
+                SaleDataGrid.DataContext = ds;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+            }
+
+
+        }
+    }
+}
